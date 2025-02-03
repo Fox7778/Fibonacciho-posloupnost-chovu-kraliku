4b36f6452e1b359950dfde76cb798df34426ced5
Fox7778 <nfilip989@gmail.com>
2025-02-03 14:50:43 +0100

    Úprava

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 // Funkce pro generování Fibonacciho posloupnosti
 function generateFibonacci(n) {
-
+let a = 1, b = 1, temp;
+let i = 0;
+while (i <= n) {
+  console.log(a);
+  temp = a + b;
+  a = b;
+  b = temp;
+  i++;
+}
 }
 
 
 // Spuštění generování Fibonacciho posloupnosti s 6 prvky
-const n = 6;
-generateFibonacci(n);
+generateFibonacci(6);
