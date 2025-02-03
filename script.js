73ac5cc237af5ca1490179dfa237ac88110bcd5c
Fox7778 <nfilip989@gmail.com>
2025-02-03 15:19:04 +0100

    Hvězdičky v html

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 // Funkce pro generování Fibonacciho posloupnosti
 function generateFibonacci(n) {
+  const fibonacci = document.getElementById("fibonacci");
   let a = 1, b = 1, temp;
   let i = 0;
   while (i <= n) {
+    const mesic = document.createElement("div");
+    mesic.classList.add("rabbit");
    //console.log(a);
    let stars ="";
    let j = 0;
    while(j < a){
-    stars+="*";
+    stars+="**";
     j++;
    }
-   console.log(stars);
+   mesic.innerHTML = stars;
+   fibonacci.appendChild(mesic);
    temp = a + b;
    a = b;
    b = temp;
