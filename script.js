0b9a08a7e6215df383757ef43f44e4b29071185d
Fox7778 <nfilip989@gmail.com>
2025-02-03 15:05:55 +0100

    Upgrade s hvězdičkami

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 // Funkce pro generování Fibonacciho posloupnosti
 function generateFibonacci(n) {
-let a = 1, b = 1, temp;
-let i = 0;
-while (i <= n) {
-  console.log(a);
-  temp = a + b;
-  a = b;
-  b = temp;
-  i++;
-}
+  let a = 1, b = 1, temp;
+  let i = 0;
+  while (i <= n) {
+   //console.log(a);
+   let stars ="";
+   let j = 0;
+   while(j < a){
+    stars+="*";
+    j++;
+   }
+   console.log(stars);
+   temp = a + b;
+   a = b;
+   b = temp;
+   i++;
+   }
 }
 
 
